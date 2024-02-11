a6a1c041a2528b80558cada7c20b556c8512ceea
maximlishchenko <makslis2000@gmail.com>
2024-02-11 23:09:05 +0000

    create rules for sosa:Observation, peco:EmissionGenerationActivity, peco:EmissionCalculationActivity, peco:EmissionCalculationEntity

--- src/main/java/uk/max/validator/TestService.java
+++ src/main/java/uk/max/validator/TestService.java
@@ -1,36 +1,31 @@
 package uk.max.validator;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.jena.graph.Graph;
-import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.shacl.ShaclValidator;
 import org.apache.jena.shacl.Shapes;
 import org.apache.jena.shacl.ValidationReport;
-import org.apache.jena.shacl.lib.ShLib;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.apache.jena.ontology.OntModel;
 import org.springframework.stereotype.Service;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 @Service
 public class TestService {
     public static void test() {
 
-        Graph shapesGraph = RDFDataMgr.loadGraph("rules.ttl", Lang.TTL);
-        Graph dataGraph = RDFDataMgr.loadGraph("provenance_trace.json", Lang.JSONLD);
+        Graph rules = RDFDataMgr.loadGraph("./rules/rules.ttl", Lang.TTL);
 
-        Shapes shapes = Shapes.parse(shapesGraph);
+        Graph dataGraph = RDFDataMgr.loadGraph("./data/provenance_trace.json", Lang.JSONLD);
+
+
+        Shapes shapes = Shapes.parse(rules);
 
         ValidationReport report = ShaclValidator.get().validate(shapes, dataGraph);
-//        ShLib.printReport(report);
-//        System.out.println();
-        RDFDataMgr.write(System.out, report.getModel(), Lang.TTL);
+
+        boolean conforms = report.conforms();
+        if (conforms) {
+            System.out.println("No constraints violated");
+        } else {
+            RDFDataMgr.write(System.out, report.getModel(), Lang.TTL);
+        }
     }
 }
