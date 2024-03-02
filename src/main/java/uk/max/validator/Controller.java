588eea6621155125aa4cfae409300971d2d272b6
maximlishchenko <makslis2000@gmail.com>
2024-03-02 18:30:16 +0000

    handle wrong file name exception in controller, add tests for SMLI traces

--- src/main/java/uk/max/validator/Controller.java
+++ src/main/java/uk/max/validator/Controller.java
@@ -1,11 +1,16 @@
 package uk.max.validator;
 
+import org.apache.coyote.Response;
 import org.apache.jena.ext.xerces.util.URI;
+import org.apache.jena.riot.RiotNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Controller {
@@ -14,17 +19,32 @@ public class Controller {
     Service service;
 
     @PostMapping("/validate-cardinality")
-    public String validateCardinality(@RequestBody String provenanceTraceName) {
-        return service.validateCardinality(provenanceTraceName);
+    public ResponseEntity<String> validateCardinality(@RequestBody String provenanceTraceName) {
+        try {
+            String result = service.validateCardinality(provenanceTraceName);
+            return ResponseEntity.ok(result);
+        } catch (RiotNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found");
+        }
     }
 
     @PostMapping("/validate-type")
-    public String validateType(@RequestBody String provenanceTraceName) {
-        return service.validateType(provenanceTraceName);
+    public ResponseEntity<String> validateType(@RequestBody String provenanceTraceName) {
+        try {
+            String result = service.validateType(provenanceTraceName);
+            return ResponseEntity.ok(result);
+        } catch (RiotNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found");
+        }
     }
 
     @PostMapping("/validate-sparql")
-    public String validateSparql(@RequestBody String provenanceTraceName) {
-        return service.validateSparql(provenanceTraceName);
+    public ResponseEntity<String> validateSparql(@RequestBody String provenanceTraceName) {
+        try {
+            String result = service.validateSparql(provenanceTraceName);
+            return ResponseEntity.ok(result);
+        } catch (RiotNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found");
+        }
     }
 }
--- src/test/java/uk/max/validator/ValidatorApplicationTests.java
+++ src/test/java/uk/max/validator/ValidatorApplicationTests.java
@@ -1,16 +1,34 @@
 package uk.max.validator;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 @SpringBootTest
 class ValidatorApplicationTests {
+
 	@Test
-	void testValidateCardinality() {
-		Service service = new Service();
-		String result = service.validateCardinality("pesticides_trace_valid");
-		assertEquals("No violations", result);
+	public void testValidate_SMLI_Trace1() {
+		assertEquals("No violations", Service.validateCardinality("smli_trace1_valid"));
+		assertEquals("No violations", Service.validateType("smli_trace1_valid"));
+		assertEquals("No violations", Service.validateSparql("smli_trace1_valid"));
+	}
+
+	@Test
+	public void testValidate_SMLI_Trace2() {
+		assertEquals("No violations", Service.validateCardinality("smli_trace2_valid"));
+		assertEquals("No violations", Service.validateType("smli_trace2_valid"));
+		String res = Service.validateSparql("smli_trace2_valid");
+		JsonArray jsonArray = new Gson().fromJson(res, JsonArray.class);
+		assertEquals(1, jsonArray.size());
+		JsonObject jsonObject = jsonArray.get(0).getAsJsonObject();
+		assertEquals("https://w3id.org/ecfkg/i/UK/BEIS/2022/CF_2127", jsonObject.get("focusNode").getAsString());
+		assertEquals("ecfo:hasApplicablePeriod", jsonObject.get("resultPath").getAsString());
+		assertEquals("sh:Warning", jsonObject.get("resultSeverity").getAsString());
 	}
 }
